5267ea83d6a5362967789e8831a32fe456739d32
Henk Kampman <henk.kampman@secondmove.com>
2025-09-16 14:02:53 +0200

    Initial Commit

---
+++ FormTest/FormTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormTestApp.swift
+//  FormTest
+//
+//  Created by Henk on 16/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct FormTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
